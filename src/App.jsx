0a7709ae44edcfeb89903fa3f1054b364e1709a8
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-03 19:49:02 +0600

    Code for Video 39-2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
+
   
+    /* Code For Video 39-2 */
+  
+  function handleClick() {
+    alert("Button Clicked");
+  }
+
+  function handleClick2() {
+    alert("Button Clicked 2");
+  }
+
+
+    const addToFive = (num) => {
+      alert (num + 5);
+    }
+
 
   return (
     <>
-      
       <h3>React Core concepts 2</h3>
-      
+
+
+      {/* Code For Video 39-2 */}
+
+
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click2</button>
+
+      <button onClick={() => alert("Button 3 clicked") }>Third</button>
+
+
+      <button onClick={() => addToFive(10)}> Button Four</button>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
